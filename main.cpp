174ebf60d82098fedbe25b4604f07f5fe9751b76
augusto <gudehh@gmail.com>
2019-07-30 15:33:01 -0300

    ands_probs leitura da pasta de fora Test set bug fix tentando remover dangling nodes adicionando tratamento de constantes
    Adições:
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     string file_name;
-    file_name="../A2.aig";
+    file_name="../A1.aig";
 //    file_name="A1_ANDs_removed_1.aig";
 //    file_name="andre.aig";
 //    file_name="andre_ANDs_removed_1.aig";
@@ -32,23 +32,23 @@ int main(int argc, char** argv) {
     mnist mnist_obj;
     graph graph_obj;
     
-    for(float th=0.0001;th<0.001;th=th+0.0001)
-    {
-//        float th=0.001;
+//    for(float th=0.0001;th<0.001;th=th+0.0001)
+//    {
+        float th=0.001;
         graph_obj.clearCircuit();
         graph_obj.setThrehsold(th);
         
-        mnist_obj.clearMnist();
-        read_mnist.close();
-        read_mnist.open("../train-images.idx3-ubyte",ifstream::binary);
-        mnist_obj.readIdx(read_mnist,"../train-images.idx3-ubyte");
-        mnist_obj.setBitsProbabilities(read_mnist);
+//        mnist_obj.clearMnist();
+//        read_mnist.close();
+//        read_mnist.open("../train-images.idx3-ubyte",ifstream::binary);
+//        mnist_obj.readIdx(read_mnist,"../train-images.idx3-ubyte");
+//        mnist_obj.setBitsProbabilities(read_mnist);
         
         graph_obj.readAIG(read,file_name);
 
 
     //    graph_obj.setANDsProbabilities(mnist_obj);
-        graph_obj.propagateAndDeleteAll(mnist_obj);
+//        graph_obj.propagateAndDeleteAll(mnist_obj);
 //        graph_obj.applyMnistRecursive(mnist_obj);
         
 
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
     //        graph_obj.findAny(node_name)->printNode();
     //    }
 //        graph_obj.~graph();
-    }
+//    }
     return 0;
 }
 

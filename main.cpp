7edbee1ecce12392fe620731fbc70b56cb359be3
augusto <gudehh@gmail.com>
2019-07-30 14:07:03 -0300

    Test set bug fix tentando remover dangling nodes adicionando tratamento de constantes
    Adições:
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- main.cpp
+++ main.cpp
@@ -32,9 +32,9 @@ int main(int argc, char** argv) {
     mnist mnist_obj;
     graph graph_obj;
     
-//    for(float th=0.0001;th<0.001;th=th+0.0001)
-//    {
-        float th=0.001;
+    for(float th=0.0001;th<0.001;th=th+0.0001)
+    {
+//        float th=0.001;
         graph_obj.clearCircuit();
         graph_obj.setThrehsold(th);
         
@@ -48,15 +48,15 @@ int main(int argc, char** argv) {
 
     //    graph_obj.setANDsProbabilities(mnist_obj);
         graph_obj.propagateAndDeleteAll(mnist_obj);
-        graph_obj.applyMnistRecursive(mnist_obj);
+//        graph_obj.applyMnistRecursive(mnist_obj);
         
 
-//        mnist_obj.clearMnist();
-//        read_mnist.close();
-//        read_mnist.open("../t10k-images.idx3-ubyte",ifstream::binary);
-//        mnist_obj.readIdx(read_mnist,"../t10k-images.idx3-ubyte");
-//        mnist_obj.setBitsProbabilities(read_mnist);
-//        graph_obj.applyMnistRecursive(mnist_obj);
+        mnist_obj.clearMnist();
+        read_mnist.close();
+        read_mnist.open("../t10k-images.idx3-ubyte",ifstream::binary);
+        mnist_obj.readIdx(read_mnist,"../t10k-images.idx3-ubyte");
+        mnist_obj.setBitsProbabilities(read_mnist);
+        graph_obj.applyMnistRecursive(mnist_obj);
 
         
     //    while(true)
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
     //        graph_obj.findAny(node_name)->printNode();
     //    }
 //        graph_obj.~graph();
-//    }
+    }
     return 0;
 }
 

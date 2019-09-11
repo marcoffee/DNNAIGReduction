d6381d9c9de2cfe6ce03b2a0bc2c50efa30258ea
augusto <gudehh@gmail.com>
2019-09-11 11:25:14 -0300

    A3 estranho, removido constantes do graph
    Adições:
    -depth calcul.
    -A4
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     string file_name;
-    file_name="../A3_ABC.aig";
+    file_name="../A1.aig";
 //    file_name="A1_ANDs_removed_1.aig";
 //    file_name="andre.aig";
 //    file_name="andre_ANDs_removed_1.aig";
@@ -43,24 +43,24 @@ int main(int argc, char** argv) {
     mnist_obj.setBitsProbabilities(read_mnist);
 
     graph_obj.readAIG(read,file_name);
-//    graph_obj.writeProbsHistogram();
+    graph_obj.writeProbsHistogram();
     
 //    graph_obj.propagateAndDeleteAll(mnist_obj);
 //    graph_obj.propagateAndDeletePIBased(mnist_obj);
 //    graph_obj.setDepthsInToOut();
-        graph_obj.setANDsProbabilities(mnist_obj);
+//        graph_obj.setANDsProbabilities(mnist_obj);
 
 //    graph_obj.applyMnistRecursive(mnist_obj);
 
 
-    mnist_obj.clearMnist();
-    read_mnist.close();
-    read_mnist.open("../t10k-images.idx3-ubyte",ifstream::binary);
-    mnist_obj.readIdx(read_mnist,"../t10k-images.idx3-ubyte");
-    mnist_obj.setBitsProbabilities(read_mnist);
-    graph_obj.applyMnistRecursive(mnist_obj);
-    
-    
+//    mnist_obj.clearMnist();
+//    read_mnist.close();
+//    read_mnist.open("../t10k-images.idx3-ubyte",ifstream::binary);
+//    mnist_obj.readIdx(read_mnist,"../t10k-images.idx3-ubyte");
+//    mnist_obj.setBitsProbabilities(read_mnist);
+//    graph_obj.applyMnistRecursive(mnist_obj);
+//    
+//    
 //    for(float th=0.0001;th<=0.001;th=th+0.0001)
 //    {
 //      

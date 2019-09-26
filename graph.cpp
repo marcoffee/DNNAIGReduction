f5eb2a6619b532043bbe84eb60f0322d9f698a2d
gudeh <gudehh@gmail.com>
2019-09-26 15:51:13 -0300

    adicionando constantes do A3 rodando train + test set, varios thresholds TESET SET ultimo bug fix2 tentando remover dangling nodes adicionando tratamento de constantes
    Adições:
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- graph.cpp
+++ graph.cpp
@@ -1833,7 +1833,7 @@ void graph::propagateAndDeleteAll(mnist& mnist_obj,int option,float min_th) {
     for(probs_it=ANDs_probabilities.begin();probs_it!=ANDs_probabilities.end();probs_it++)
     {
         and_ptr=&all_ANDS.find(probs_it->first)->second;
-        if(probs_it->second<= 1- new_ths[and_ptr->getDepth()])
+        if(probs_it->second<= 1- new_ths[this->all_depths[probs_it->first/2]])
 //        if(probs_it->second<= threshold)
         {
 #if DEBUG >=2
@@ -1858,7 +1858,7 @@ void graph::propagateAndDeleteAll(mnist& mnist_obj,int option,float min_th) {
 #endif
            zero_count++;
         }        
-        if(probs_it->second<= new_ths[and_ptr->getDepth()])
+        if(probs_it->second<= new_ths[this->all_depths[probs_it->first/2]])
 //        if(probs_it->second>= 1-threshold)
         {
 #if DEBUG >=2

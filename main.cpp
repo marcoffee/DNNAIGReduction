c6fc41aedacfaa357bbcd5640f23d54dd341c00b
gudeh <gudehh@gmail.com>
2019-11-28 03:56:59 -0200

    arrumando nodos removidos por level adicionando simplificacao com abc Adicionando structural hash Adicionando contagem de nodos removidos com certo depth Esquema pra passar no ABC, achei um erro Comecando terceiro metodo A3 estranho, removido constantes do graph
    Adições:
    -depth calcul.
    -A4
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void abcWrite(string new_name,string abc_name){
     ofstream script("script.scr");
-    script<<"&r "<<new_name<<".aig"<<endl<<"&ps"<<endl<<"&w "<<abc_name<<".aig"<<endl<<"quit";
+    script<<"&r "<<new_name<<".aig"<<endl<<"&ps"<<endl<<"&w "<<abc_name<<endl<<"quit";
     script.close();
     system("./../abc -c 'source script.scr' >> log.txt ");
 }

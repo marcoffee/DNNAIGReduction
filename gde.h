8f363179d68cd47775b8bbac08f2179b227b06a6
gudeh <gudehh@gmail.com>
2019-12-28 14:17:49 -0300

    Adicionado size e LD pro ABC, Bug fix exec_times, adicionado argv pro script do screen, Esquema pra passar no ABC (matendo os PIs removidos) IF 1% no sigmoid Comecando terceiro metodo Adicionando contagem de nodos removidos com certo depth A3 estranho, removido constantes do graph
    Adições:
    -depth calcul.
    -A4
    -Propagação de bits em vetor feito.
    -propagateandeletev2 virou titular
    -apply image AUX struct REFEITO (continua mais lento que recursivo)
    -Cut AIG adicionado
    -Remocao de nodos REFEITA.
    -adicionado varias execucoes na mesma chamada.
    
    Bug fix: 
    -Binario para decimal arrumado
    -ordem correta dos inputs dos nodos AND
    -adicionado remocao de POs (arrumado)
    -applymnist bombando

--- gde.h
+++ gde.h
@@ -30,8 +30,8 @@
 //#define LEAVE_CONSTANTS 0
 #define USE_ABC 0
 #define FIX_DOUBLED_NODES 1
-#define EXECUTE_ONCE 0
-#define APPLY_MNIST 0
+#define EXECUTE_ONCE 1
+#define APPLY_MNIST 1
 #define CEC 0
 #define WRITE_AIG 0
 #define WRITE_AAG 0

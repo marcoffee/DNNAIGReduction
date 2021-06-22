2a3a14a0d5c6743b3570f5f9b153380c29149498
gudeh <gudehh@gmail.com>
2021-06-21 21:17:28 -0300

    server run update

--- Code/AIG.cpp
+++ Code/AIG.cpp
@@ -90,7 +90,7 @@ void abcWrite(string old_name,string new_name){
     string command;
     ofstream log(abc_log_name); log.close();
     command="-c '&r "+old_name+".aig' -c '&ps' -c '&w "+new_name+"' -c 'quit'";
-    cout<<"Calling ABC, command:"<<command<<endl
+    cout<<"Calling ABC, command:"<<command<<endl;
     system(command.c_str());
 //    script<<"&r "<<old_name<<".aig"<<endl<<"&ps"<<endl<<"&w "<<new_name<<endl<<"quit";
 //    script.close();
